12761fcc6826593079f2391ae6fe803b07406a95
Bohdan Bilyk <bohdanbilykwork@gmail.com>
2023-05-29 17:47:14 +0300

    remove comments

--- task_1/src/logger.js
+++ task_1/src/logger.js
@@ -1,17 +1,20 @@
 const fs = require('fs');
 const path = require('path');
 
-const logFilePath = path.join(__dirname, 'app.log'); // Specify the path and filename for the log file
+const logFilePath = path.join(__dirname, 'app.log');
 
-function redirectConsoleLogs() {
+const getDatePrefix = () => {
+  const now = new Date();
+  return `[${now.toLocaleDateString()} ${now.toLocaleTimeString()}]`;
+}
+
+const redirectConsoleLogs = () => {
   const logStream = fs.createWriteStream(logFilePath, { flags: 'a' });
 
-  // Save original console methods for later use
   const originalConsoleLog = console.log;
   const originalConsoleError = console.error;
   const originalConsoleWarn = console.warn;
 
-  // Override console methods to redirect logs to file
   console.log = (...args) => {
     originalConsoleLog(...args);
     logStream.write(`[LOG] ${getDatePrefix()} ${args.join(' ')}\n`);
@@ -27,15 +30,9 @@ function redirectConsoleLogs() {
     logStream.write(`[WARN] ${getDatePrefix()} ${args.join(' ')}\n`);
   };
 
-  // Close the log stream when the app exits
   process.on('exit', () => {
     logStream.end();
   });
-}
-
-function getDatePrefix() {
-  const now = new Date();
-  return `[${now.toLocaleDateString()} ${now.toLocaleTimeString()}]`;
 }
 
 module.exports = {
